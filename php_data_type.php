8a97e2ad4b22fa5937352b347a9e28e5d8c306c2
beki255 <bereket2553@gmail.com>
2026-02-23 23:08:08 +0300

    php

--- php_data_type.php
+++ php_data_type.php
@@ -4,4 +4,13 @@ $b=24;
 $c=10.5;
 $d=NULL;
 $e=array("c++","java","php","python");
+var_dump($a);
+echo "</br>";
+var_dump($b);
+echo "</br>";
+var_dump($c);
+echo "</br>";
+var_dump($d);
+echo "</br>";
+var_dump($e);
 ?>
