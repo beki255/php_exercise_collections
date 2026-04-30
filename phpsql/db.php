f78d2a00780107aa854ca54c9bb4f9b9c1403cf1
beki255 <bereket2553@gmail.com>
2026-04-30 13:37:44 +0300

    updated

--- phpsql/db.php
+++ phpsql/db.php
@@ -3,7 +3,6 @@ $host="localhost";
 $user="root";
 $pass="";
 $db="DBU3";
-$db="DBU";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     die("connection failed:".$conn->connect_error);
