719ff2d5c5bef89f9bf679479f62235e96796443
beki255 <bereket2553@gmail.com>
2026-04-30 09:52:51 +0300

    update

---
+++ sampleproject/news.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>beki tech</title>
+</head>
+<body>
+      <?php
+    include "header.php";
+    ?>
+    <h3>news page</h3>
+        <div>
+        <a href="indexx.php">Home</a>
+        <a href="news.php">News</a>
+        <a href="about.php">about</a>
+    </div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Nemo dignissimos neque exercitationem, tempore asperiores 
+    ullam ducimus molestiae cumque nobis. Ratione, nisi cupiditate 
+    debitis vero animi minima nam explicabo dolor quaerat!
+     <?php
+    include "footer.php";
+    ?>
+</body>
+</html>
