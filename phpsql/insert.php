03423e1de4be23e4b507933936599a2326ae12f2
beki255 <bereket2553@gmail.com>
2026-04-30 13:36:51 +0300

    insert.php is added

---
+++ phpsql/insert.php
@@ -0,0 +1,20 @@
+<?php include "db.php";?>
+<form method="POST">
+    ID:<input type="number" name="id"><br>
+    Name:<input type="text" name="name"><br>
+    Age:<input type="number" name="age"><br>
+    Gender:<input type="text" name="gender"><br>
+    <button type="submit" name="submit">insert</button>
+</form>
+<?php
+if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $name=$_POST['submit'];
+    $age=$_POST['age'];
+    $gender=$_POST['gender'];
+    $stmt = $conn->prepare("INSERT INTO student VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("isis", $id, $name, $age, $gender);
+    $stmt->execute();
+    echo "Student inserted successfully!";
+  }
+?>
