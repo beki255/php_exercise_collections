8069dd1ecf384769d162f435040ff3ecc4f1f54e
beki255 <bereket2553@gmail.com>
2026-04-30 09:52:39 +0300

    update

---
+++ sampleproject/indexx.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>beki tech</title>
+</head>
+<body>
+    <?php
+    require "header.php";
+    ?>
+    <h3>home page</h3>
+    <div>
+        <a href="indexx.php">Home</a>
+        <a href="news.php">News</a>
+        <a href="about.php">about</a>
+    </div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ipsa ea animi ipsum maxime asperiores debitis veritatis, voluptatibus laboriosam pariatur expedita velit, nobis omnis molestiae, voluptatum laborum ex possimus perferendis.
+    <?php
+    require "footer.php";
+    ?>
+</body>
+</html>
