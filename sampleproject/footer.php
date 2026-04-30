e9edb2ae61d495f92e4352f54f8d5822cfba3b15
beki255 <bereket2553@gmail.com>
2026-04-30 09:52:08 +0300

    update

---
+++ sampleproject/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> <br>
+    &copy;2024-<?php echo date ("Y")?>; beki tech all right reserved!
+</body>
+</html>
