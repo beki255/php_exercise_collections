c41046c30b44c3207eb840d8aaf8e01e596cf9c6
beki255 <bereket2553@gmail.com>
2026-04-30 14:30:10 +0300

    add css

--- phpsql/view.php
+++ phpsql/view.php
@@ -1,3 +1,53 @@
+
+<?php include 'db.php'; ?>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Gender</th>
+    </tr>
+<?php
+    $result = $conn->query("SELECT * FROM student");
+    while ($row = $result->fetch_assoc()) {
+    echo "<tr>
+    <td>".htmlspecialchars($row['id'])."</td>
+    <td>".htmlspecialchars($row['name'])."</td>
+    <td>".htmlspecialchars($row['age'])."</td>
+    <td>".htmlspecialchars($row['gender'])."</td>
+    </tr>";
+}
+?>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- 
 <?php include 'db.php'; ?>
 <!DOCTYPE html>
@@ -81,49 +131,3 @@ if ($result && $result->num_rows > 0) {
 
 </body>
 </html> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php include 'db.php'; ?>
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Gender</th>
-    </tr>
-<?php
-    $result = $conn->query("SELECT * FROM student");
-    while ($row = $result->fetch_assoc()) {
-    echo "<tr>
-    <td>".htmlspecialchars($row['id'])."</td>
-    <td>".htmlspecialchars($row['name'])."</td>
-    <td>".htmlspecialchars($row['age'])."</td>
-    <td>".htmlspecialchars($row['gender'])."</td>
-    </tr>";
-}
-?>
-</table>
